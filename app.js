1648f209bc85ab4859187926a41414f0485b7dbf
Nick Dobbs <68311724+nedobbs1@users.noreply.github.com>
2020-12-07 20:34:13 -0500

    Setup local DB for composing posts

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -21,13 +22,34 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-let posts = [];
+mongoose.connect('mongodb://localhost/blogDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+mongoose.set('useFindAndModify', false);
+
+
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+
 
 //GET REQUESTS
 app.get("/", function (req, res) {
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts,
+  Post.find({}, function (err, posts) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", {
+        startingContent: homeStartingContent,
+        posts: posts,
+      });
+    }
   });
 });
 
@@ -47,15 +69,15 @@ app.get("/compose", function (req, res) {
   res.render("compose")
 });
 
-app.get("/posts/:postName", function (req, res) {
 
-  const requestedTitle = _.lowerCase(req.params.postName);
+app.get("/posts/:postID", function (req, res) {
 
-  posts.forEach(function (post) {
+  const requestedPostId = req.params.postID;
 
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
+  Post.findOne({
+    _id: requestedPostId
+  }, function (err, post) {
+    if (!err) {
       res.render("post", {
         title: post.title,
         content: post.content
@@ -69,12 +91,17 @@ app.get("/posts/:postName", function (req, res) {
 
 //POST REQUESTS
 app.post("/compose", function (req, res) {
-  const post = {
+
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
-  posts.push(post);
-  res.redirect("/");
+  });
+
+  post.save(function (err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 });
 
 
